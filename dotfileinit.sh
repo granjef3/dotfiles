9e90f4082a14e1805d59afc506cf3dbf3732ef17
Matthew Alonso <granjef3@gmail.com>
2019-07-23 15:08:21 -0500

    updated dotfileinit

--- dotfileinit.sh
+++ dotfileinit.sh
@@ -2,5 +2,6 @@ git init --bare $HOME/.cfg
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 config config --local status.showUntrackedFiles no
 echo "alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'" >> $HOME/.zshrc
+echo "added alias to zshrc"
 source $HOME/.zshrc
 config remote add origin git@github.com:granjef3/dotfiles.git
