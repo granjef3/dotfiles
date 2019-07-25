d9b5d1efe90a398fd6c7a9434299728c4dacbf27
Matthew Alonso <granjef3@gmail.com>
2019-07-25 15:46:28 -0500

    added update alias

--- dotfileinit.sh
+++ dotfileinit.sh
@@ -1,10 +1,9 @@
-git init --bare $HOME/.cfg
-echo "created git repo"
+git init --bare $HOME/.cfg && echo "created git repo"
 
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 config config --local status.showUntrackedFiles no
-echo "alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'" >> $HOME/.zshrc
+config config --local alias.update 'add -u'
+echo "alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'" >> $HOME/.zshrc && echo "added alias"
 
 source $HOME/.zshrc
-config remote add origin git@github.com:granjef3/dotfiles.git
-echo "added remote. Done!"
+config remote add origin git@github.com:granjef3/dotfiles.git && echo "added remote. Done!"
